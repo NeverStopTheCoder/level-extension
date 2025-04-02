b6add6ec4996223baeff47343ce529477c7a5d25
NeverStopTheCoder <jenny_fullerton@yahoo.com>
2025-04-02 08:34:45 -0400

    Initial files for MakeCode project

---
+++ Uu.ts
@@ -0,0 +1,82 @@
+// Add your code here
+//%icon=""
+//%color=#000080
+namespace levels {
+    export enum Levels {
+        One,
+        Two,
+        Three,
+        Four
+    }
+    let level = 0
+//%block="set level $l to $tile"
+//%tile.shadow=tiles_tilemap_editor
+export function c(tile: tiles.TileMapData,l:Levels): void {
+forever(function() {
+if (l === 0) {
+if (level === 1) {
+tiles.setCurrentTilemap(tile)
+}
+
+}else if (l == 1) {
+if (level === 2) {
+tiles.setCurrentTilemap(tile)
+}
+} 
+
+else if (l == 2) {
+if (level === 3) {
+tiles.setCurrentTilemap(tile)
+}
+}
+
+else if (l == 3)  {
+if (level === 4) {
+tiles.setCurrentTilemap(tile)
+}
+}
+})
+
+}
+   
+     //% block="set level to $e"
+    //% cover.shadow=tileset_tile_picker
+    //%sprite.shadow=variables_get
+    export function coverTil(e:Levels) {
+     if (e === 0) {
+         level = 1
+     } else if (e === 1) {
+         level = 2
+     }else if (e === 2) {
+         level = 3
+     }else if (e === 3) {
+         level = 4
+     }
+    }
+    //%block="Level is $l"
+    export function b(l: Levels): boolean {
+        if (l === 0) {
+            if (level === 1) {
+              return true;
+            }
+
+        } else if (l == 1) {
+            if (level === 2) {
+              return true;
+            }
+        }
+
+        else if (l == 2) {
+            if (level === 3) {
+                return true;
+            }
+        }
+
+        else if (l == 3) {
+            if (level === 4) {
+                return true;
+            }
+        }
+        return false
+    }
+}
