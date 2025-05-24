ccafdecc688933a1a06742b5c17b1ed58fac03cc
NeverStopTheCoder <jenny_fullerton@yahoo.com>
2025-05-24 10:00:27 -0400

    Update Uu.ts, tilemap.g.jres, tilemap.g.ts

--- Uu.ts
+++ Uu.ts
@@ -9,8 +9,11 @@ namespace levels {
         Four
     }
     let level = 0
+    let level2 = 0
+    let l2 = ""
 //%block="set level $l to $tile"
 //%tile.shadow=tiles_tilemap_editor
+//%group=Basic
 export function c(tile: tiles.TileMapData,l:number): void {
 forever(function() {
 if (l === l) {
@@ -20,18 +23,42 @@ tiles.setCurrentTilemap(tile)
 }
 })
 }
-
+//%block="set level $s to $n as $t"
+//%group="Advanced"
+//%t.shadow=tiles_tilemap_editor
+//%s.defl=Dungeon
+export function c2(t: tiles.TileMapData,n: number,s: string): void {
+forever(function() {
+if (n === n && s === s) {
+    if (level2 === n && l2 === s) { 
+tiles.setCurrentTilemap(t)
+}
+}
+})
+}
    
      //% block="set level to $e"
     //% cover.shadow=tileset_tile_picker
     //%sprite.shadow=variables_get
+    //%group=Basic
     export function coverTil(e:number) {
      if (e === e) {
          level = e
          pause(50)
      } 
     }
+    //%block="set level to $n as $s"
+    //%group="Advanced"
+    //%s.defl=Dungeon
+    export function coverTil2(n: number,s:string): void {
+        if (n === n && s === s) {
+            level2 = n
+            l2 = s
+            pause(50)
+        }
+    }
     //%block="Level is $l"
+    //%group=Basic
     export function b(l: number): boolean {
         if (l === l) {
             if (level === l) {
@@ -39,6 +66,17 @@ tiles.setCurrentTilemap(tile)
             }
         }
         return false
+    }
+    //%block="Level is $n and $s"
+    //%group="Advanced"
+    //%s.defl=Dungeon
+    export function b2(n: number,s:string): boolean {
+        if (n === n && s === s) {
+            if (level2 === n && l2 === s) {
+                return true;
+            }
+        }
+        return false;
     }
     //block="set $sprite spot to $tile for level $l"
     //tile.shadow=tileset_tile_picker
